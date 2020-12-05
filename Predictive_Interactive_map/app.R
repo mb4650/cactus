a9f46102dd570a47665d039e59cc741fd5b5af55
Gauri Bhatkhande <gauribhatkhande@gauris-mbp.dyn.nyp.org>
2020-12-04 21:22:28 -0500

    added negative poisson model with 2nd set of variables

--- Predictive_Interactive_map/app.R
+++ Predictive_Interactive_map/app.R
@@ -30,8 +30,12 @@ table_lat_long_df =
   first() %>%
   html_table()
 
-neg_bin_mod = glm.nb(new_cases ~ as.factor(month) + state_name + state_tavg + state_total_prcp,
+neg_bin_mod = glm.nb(new_cases ~ month_name + state_name + state_tavg + state_total_prcp,
                      data = covid_noaa_dataset)
+
+neg_bin_policy = glm.nb(new_cases ~ as.factor(month) + state_name + school_closing + workplace_closing + gatherings_restrictions, 
+                        
+                        data = covid_noaa_dataset) 
 
 ui <- fluidPage(
   titlePanel("Case count Predictor"),
@@ -42,10 +46,10 @@ ui <- fluidPage(
           helpText(h2("Enter details")),
           
           selectInput("Month1", h3("Select Month"),
-                      choices =unique(covid_noaa_dataset["month"]),selected = 4),
+                      choices =unique(covid_noaa_dataset["month_name"]),selected = "May"),
           
           selectInput("State", h3("Select state"),
-                      choices =unique(covid_noaa_dataset["state_name"]),selected = 10),
+                      choices =unique(covid_noaa_dataset["state_name"]),selected = "New York"),
           
           sliderInput( "tavg",
                        label = "Select Average temperature",
@@ -86,7 +90,49 @@ ui <- fluidPage(
                leafletOutput("plot2")
              )
              
-    )
+    ),
+    tabPanel('Map3',
+             sidebarPanel(
+               helpText(h2("Enter details")),
+               
+               selectInput("month3", h3("Select Month"),
+                           choices =unique(covid_noaa_dataset["month"]),selected = 4),
+               
+               selectInput("state1", h3("Select State"),
+                           choices =unique(covid_noaa_dataset["state_name"]),selected = 10),
+               
+               selectInput("school", h3("Select School Policy"),
+                           choices = list("No measures" = 0, "Recommended closing" = 1,
+                                          "Require closing some schools" = 2, "Require closing all schools" = 3),
+                          selected = 0),
+               
+               selectInput("workplace", h3("Select Workplace Policy"),
+                           choices = list("No measures" = 0, "Recommended work from home" = 1,
+                                          "Require closing some sectors" = 2, "Only essential sectors open" = 3),
+                           selected = 0),
+               
+               selectInput("gathering", h3("Select Gathering Policy"),
+                           choices = list("No restrictions" = 0, "Limit very large gatherings" = 1,
+                                          "Limit upto 100-1000 people " = 2, "Limit upto 10-100 people" = 3, 
+                                          "Limit upto 10 people"),
+                           selected = 0),
+               
+               sliderInput( "tavg1",
+                            label = "Select Average temperature",
+                            min = 1,
+                            max = max(covid_noaa_dataset["state_tmax"])+20,
+                            value = 30),
+               
+             ),
+             
+             mainPanel(
+               # fluidRow(
+               #   column(12,label = "Prediction", leafletOutput("selected_var")),
+               #   column(12, label = "general trend", leafletOutput("plot1"))
+               # )
+               leafletOutput("plot3")
+             )
+      )
   )
 )
 
@@ -95,7 +141,7 @@ server <- function(input, output) {
   
   output$plot1 <- renderLeaflet({
     
-    df = data.frame( month = as.factor(input$Month1), state_name = input$State ,
+    df = data.frame( month_name = as.factor(input$Month1), state_name = input$State ,
                      state_tavg = input$tavg , state_total_prcp = input$prcp )
     
     count = as.integer( predict(neg_bin_mod, df, type = "response") )
@@ -111,9 +157,9 @@ server <- function(input, output) {
              state_name ==input$State)
     
     
-    map_df = inner_join(map_df, table_lat_long_df, by = c("key_alpha_2" = "state" ))
+    map_df = inner_join(map_df, table_lat_long_df, by = c("state" = "state" ))
     
-    states_merged_sb <- geo_join(states, map_df, "STUSPS", "key_alpha_2")
+    states_merged_sb <- geo_join(states, map_df, "STUSPS", "state")
     
     
     leaflet(states_merged_sb)%>%
@@ -142,7 +188,9 @@ server <- function(input, output) {
   output$plot2 <- renderLeaflet({
     map_df = covid_noaa_dataset%>% filter(month == as.factor(input$Month2))
     
-    map_df = inner_join(map_df, table_lat_long_df, by = c("key_alpha_2" = "state" ))
+    map_df = inner_join(map_df, table_lat_long_df, by = c("state" = "state" ))
+    
+    states_merged_sb <- geo_join(states, map_df, "STUSPS", "state")
     
     pal <- colorNumeric(
       palette = "Blues",
@@ -154,7 +202,7 @@ server <- function(input, output) {
       domain = map_df$new_cases)
     
     
-    states_merged_sb <- geo_join(states, map_df, "STUSPS", "key_alpha_2")
+   
     
     labels <- sprintf(
       "<strong>%s</strong><br/>%s",
@@ -192,6 +240,52 @@ server <- function(input, output) {
                 title = "temp",
                 labFormat = labelFormat(prefix = "F"),
                 opacity = 1)
+  })
+  
+  output$plot3 <- renderLeaflet({
+    
+    df = data.frame( month = as.factor(input$Month1), state_name = input$State ,
+                     state_tavg = input$tavg , state_total_prcp = input$prcp )
+    
+    count = as.integer( predict(neg_bin_mod, df, type = "response") )
+    
+    display = sprintf(
+      "<strong>Predicted number of cases: %s</strong>",
+      count
+    ) %>% lapply(htmltools::HTML)
+    
+    #paste( "you chose", a)
+    map_df = covid_noaa_dataset%>% 
+      filter(month == as.factor(input$Month1),
+             state_name ==input$State)
+    
+    
+    map_df = inner_join(map_df, table_lat_long_df, by = c("state" = "state" ))
+    
+    states_merged_sb <- geo_join(states, map_df, "STUSPS", "state")
+    
+    
+    leaflet(states_merged_sb)%>%
+      setView(-96, 40, 3.5) %>%
+      addPolygons(
+        fillColor = "pink",
+        weight = 2,
+        opacity = 1,
+        color = "white",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlight = highlightOptions(
+          weight = 5,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.7,
+          bringToFront = FALSE)) %>%
+      
+      addCircleMarkers(lat =map_df$latitude.y, lng = map_df$longitude.y,
+                       radius =count/40000,color = "red",popup =map_df$state_name) %>%
+      
+      addControl(display, position = "bottomleft")
+    
   })
   
 }
